a06d724464af0ea011326f64abb771dd8880bbcf
Abril <abrilmicaela_9@hotmail.com>
2024-11-04 22:44:55 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EncargadoComponent } from './components/encargado/encargado.component';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-root',
