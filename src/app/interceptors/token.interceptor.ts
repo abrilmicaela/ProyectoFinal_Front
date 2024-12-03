d595a95017e058843364853a14ecaf0b37edab37
Víctor <Vivivikthor@gmail.com>
2024-12-03 22:06:34 +0100

    Interceptor Funcionando!

--- src/app/interceptors/token.interceptor.ts
+++ src/app/interceptors/token.interceptor.ts
@@ -1,10 +1,17 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
-const token = localStorage.getItem('token')
-if(token){
-  const authHeaders = req.clone({headers: req.headers.set('Authorization', token)});
-  return next(authHeaders)
-}
-  return next(req);
-};
+    const token = localStorage.getItem('token')|| "";
+    const requestCopy = req.clone({
+        setHeaders: {
+            "Content-Type": "application/json",
+            "Authorization": token
+        }
+    });
+    return next(requestCopy);
+  };
+    // if(token){
+    //   const authHeaders = req.clone({headers: req.headers.set('Authorization', token)});
+    //   return next(authHeaders)
+    // }
+    //   return next(req);
